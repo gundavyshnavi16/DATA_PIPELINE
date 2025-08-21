990d600fb3a43d703198d4fc5b93e4a2d5462c7f
gundavyshnavi16 <gundavyshnavi16@gmail.com>
2025-08-21 17:57:36 -0400

    Update int_sales_by_date.sql
    comments added

--- DATA_PIPELINE/superstore_pipeline/dbt/models/int_sales_by_date.sql
+++ DATA_PIPELINE/superstore_pipeline/dbt/models/int_sales_by_date.sql
@@ -1,13 +1,18 @@
 {{ config(materialized = 'table') }}
 
+-- Here we're creating an intermediate model to calculate daily sales metrics.
+-- This helps keep our final models clean and focused.
 with sales_per_date as (
-  select
-    order_date,
-    sum(sales)    as total_sales,
-    sum(quantity) as total_quantity,
-    sum(profit)   as total_profit
-  from {{ ref('stg_orders') }}
-  group by order_date
+  select
+    -- We're summing up sales, quantity, and profit...
+    order_date,
+    sum(sales)    as total_sales,
+    sum(quantity) as total_quantity,
+    sum(profit)   as total_profit
+  from {{ ref('stg_orders') }} -- ...from our cleaned staging data.
+  -- We're grouping all of this data by the order date.
+  group by order_date
 )
 
+-- Now, we just select everything from our daily summary to create the final table.
 select * from sales_per_date
