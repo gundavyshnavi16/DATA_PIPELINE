c3d78e0cf9dd1dac6a004212354ea5d43d9d8dde
gundavyshnavi16 <gundavyshnavi16@gmail.com>
2025-08-21 15:34:26 -0400

    Update int_sales_by_date.sql

--- DATA_PIPELINE/superstore_pipeline/dbt/models/int_sales_by_date.sql
+++ DATA_PIPELINE/superstore_pipeline/dbt/models/int_sales_by_date.sql
@@ -1,12 +1,13 @@
+{{ config(materialized = 'table') }}
+
 with sales_per_date as (
-    select
-        order_date,
-        sum(sales) as total_sales,
-        sum(quantity) as total_quantity,
-        sum(profit) as total_profit
-    from {{ ref('stg_orders') }}
-    group by order_date
+  select
+    order_date,
+    sum(sales)    as total_sales,
+    sum(quantity) as total_quantity,
+    sum(profit)   as total_profit
+  from {{ ref('stg_orders') }}
+  group by order_date
 )
 
 select * from sales_per_date
-order by order_date
